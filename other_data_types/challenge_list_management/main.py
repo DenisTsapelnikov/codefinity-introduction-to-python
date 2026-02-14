59bb50e43892cdf73c030cd1be7248d87cef0b57
DenisTsapelnikov <>
2026-02-14 17:06:53 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,9 @@
+meat = ["Ham", 3.99, 50, "Sliced"]
+cheese = ["Cheddar", 5.49, 100, "Sharp"]
+condiment = ["Mustard", 1.99, 75, "Spicy"]
+
+deli_dept = [meat, cheese, condiment]
+
+if "Ham" in meat and meat[2] < 100 :
+    meat[2] = 100
+    
